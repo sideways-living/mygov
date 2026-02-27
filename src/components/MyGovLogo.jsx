dbeabe0d9f770c7b42ec6fb232725661b751f017
Dan <admin@sideways.living>
2026-02-27 12:09:29 +0000

    File changes

--- src/components/MyGovLogo.jsx
+++ src/components/MyGovLogo.jsx
@@ -1,15 +1,25 @@
 export default function MyGovLogo({ size = 28 }) {
   return (
     <svg width={size} height={size} viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
-      {/* Left rounded triangle */}
+      <defs>
+        <mask id="leftTriangleMask">
+          {/* White rectangle covering the non-overlapping part */}
+          <rect x="0" y="0" width="95" height="200" fill="white" />
+          {/* Transparent rectangle for the overlapping part */}
+          <rect x="95" y="0" width="20" height="200" fill="black" />
+        </mask>
+      </defs>
+      
+      {/* Left rounded triangle with mask */}
       <path
         d="M 20 10 Q 26 10 32 18 L 104 100 L 32 182 Q 26 190 20 190 Q 8 190 8 178 L 8 22 Q 8 10 20 10 Z"
         fill="black"
+        mask="url(#leftTriangleMask)"
       />
       
       {/* Right rounded triangle */}
       <path
-        d="M 84 10 Q 90 10 96 18 L 168 100 L 96 182 Q 90 190 84 190 Q 72 190 72 178 L 72 22 Q 72 10 84 10 Z"
+        d="M 95 10 Q 101 10 107 18 L 179 100 L 107 182 Q 101 190 95 190 Q 83 190 83 178 L 83 22 Q 83 10 95 10 Z"
         fill="black"
       />
     </svg>
