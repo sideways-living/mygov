6d1c1d9f3cbbecd79274097375545400225e4912
Dan <admin@sideways.living>
2026-02-27 12:08:06 +0000

    File changes

--- src/components/MyGovLogo.jsx
+++ src/components/MyGovLogo.jsx
@@ -3,13 +3,13 @@ export default function MyGovLogo({ size = 28 }) {
     <svg width={size} height={size} viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
       {/* Left rounded triangle */}
       <path
-        d="M 40 35 Q 45 35 50 40 L 100 100 L 50 160 Q 45 165 40 165 Q 30 165 30 155 L 30 45 Q 30 35 40 35 Z"
+        d="M 20 10 Q 26 10 32 18 L 104 100 L 32 182 Q 26 190 20 190 Q 8 190 8 178 L 8 22 Q 8 10 20 10 Z"
         fill="black"
       />
       
       {/* Right rounded triangle */}
       <path
-        d="M 100 35 Q 105 35 110 40 L 160 100 L 110 160 Q 105 165 100 165 Q 90 165 90 155 L 90 45 Q 90 35 100 35 Z"
+        d="M 84 10 Q 90 10 96 18 L 168 100 L 96 182 Q 90 190 84 190 Q 72 190 72 178 L 72 22 Q 72 10 84 10 Z"
         fill="black"
       />
     </svg>
